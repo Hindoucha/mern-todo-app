6dcab0f3ec684890debb3a890e2259387cf163ca
Hind BENSAFI <hindbensafi@gmail.com>
2022-01-23 13:59:27 +0100

    Crud operations are implemented in this backend

---
+++ models/Todo.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema
+
+const TodoSchema = schema({
+    text : {
+        type : String,
+        required : true
+    },
+    complete : {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: String,
+        default: Date.now()
+    }
+})
+
+const Todo = mongoose.model("Todo", TodoSchema)
+
+module.exports = Todo
